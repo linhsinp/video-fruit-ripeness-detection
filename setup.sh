6662ef35b97f0582eda2efc220bceff65dd07622
linhsinp <linhsinp@gmail.com>
2025-05-22 12:22:42 +0200

    update python version

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # install poetry
-curl -sSL https://install.python-poetry.org | python3 -
-export PATH="/home/{$USER}/.local/bin:$PATH" 
+curl -sSL https://install.python-poetry.org | python3.10 -
+export PATH="/home/{$USER}/.local/bin:$PATH"
 
 # # https://stackoverflow.com/questions/59882884/vscode-doesnt-show-poetry-virtualenvs-in-select-interpreter-option
 poetry config virtualenvs.in-project true
+
+# Run with project python env when setting up projects
+poetry env use $(pyenv which python)
 
 # Install project to setup requirements
 poetry install --no-interaction
